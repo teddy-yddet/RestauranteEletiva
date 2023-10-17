261ed001a5a00b01038c59ecef1898518c7ccb43
sidne <sidnei.m@aluno.ifsp.edu.br>
2023-10-16 22:35:45 -0300

    girar horizontal

--- app/src/main/java/com/example/atividade0708/FormCadastro.java
+++ app/src/main/java/com/example/atividade0708/FormCadastro.java
@@ -1,16 +1,14 @@
 package com.example.atividade0708;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class FormLogin extends AppCompatActivity {
+import android.os.Bundle;
+
+public class FormCadastro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form_login);
-
-        //getSupportActionBar().hide();
+        setContentView(R.layout.activity_form_cadastro);
     }
 }
