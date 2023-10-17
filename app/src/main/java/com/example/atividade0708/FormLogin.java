72a1bdec36474ff7cde958b9af3d96a208c39556
sidne <sidnei.m@aluno.ifsp.edu.br>
2023-10-16 22:47:38 -0300

    form login ok

--- app/src/main/java/com/example/atividade0708/FormLogin.java
+++ app/src/main/java/com/example/atividade0708/FormLogin.java
@@ -1,16 +1,33 @@
 package com.example.atividade0708;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FormLogin extends AppCompatActivity {
 
+    private TextView text_tela_cadastro;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
+        iniciarComponentes();
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FormLogin.this, FormCadastro.class);
+                startActivity(intent);
+            }
+        });
 
-        //getSupportActionBar().hide();
     }
+
+    private void iniciarComponentes() {
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+    }
+
 }
